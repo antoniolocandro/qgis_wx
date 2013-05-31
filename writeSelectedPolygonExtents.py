fdd4630291be912c5172c45a2c8f51a875e52311
Richard Burcher <richardburcher@gmail.com>
2013-05-31 19:45:11 -0400

    changed tab spacing

--- writeSelectedPolygonExtents.py
+++ writeSelectedPolygonExtents.py
@@ -1,23 +1,24 @@
 def run_script(iface):
-	"""for use in QGIS script runner plugin.
-	user selected polygons bounding coordinates extracted & output to file.
-	
-	** very basic implementation. assumes user has selected 1 polygon, has permissions to write file &
-	polygons are valid."""
-	
-	import os,sys
-	
-	outFile = "/Users/drownedfrog/Projects/wxHondrusas_antinio/outputPolygonExtents.txt"
-	
-	# must select polygon
-	layer = iface.activeLayer()
-	# only want the first feature
-	feature = layer.selectedFeatures()[0]
-	# get bounding box as string
-	bbox = feature.geometry().boundingBox().toString()
-	
-	# write contents to file
-	f = open(outFile,"w")
-	f.write(bbox)
-	f.close()
+    """for use in QGIS script runner plugin.
+    user selected polygons bounding coordinates extracted & output to file.
+    
+    ** very basic implementation. assumes user has selected 1 polygon, has permissions to write file &
+    polygons are valid."""
+    
+    import os
+    import sys
+    
+    outFile = "/Users/drownedfrog/Projects/wxHondrusas_antinio/outputPolygonExtents.txt"
+    
+    # must select polygon
+    layer = iface.activeLayer()
+    # only want the first feature
+    feature = layer.selectedFeatures()[0]
+    # get bounding box as string
+    bbox = feature.geometry().boundingBox().toString()
+    
+    # write contents to file
+    f = open(outFile,"w")
+    f.write(bbox)
+    f.close()
 
