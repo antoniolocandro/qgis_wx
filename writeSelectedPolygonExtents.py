4aa2f4aadfbb4991f5ce781b26a8dd6b8826c7d4
Richard Burcher <richardburcher@gmail.com>
2013-05-31 19:56:03 -0400

    added todo tasks

--- writeSelectedPolygonExtents.py
+++ writeSelectedPolygonExtents.py
@@ -11,14 +11,18 @@ def run_script(iface):
     # location to write text file with bounding coordinates
     bboxCoordFile = "/Users/drownedfrog/Projects/wxHondrusas_antinio/outputPolygonExtents.txt"
     
+    #TODO: validate polygon is selected. provide message with instructions.
     # must select polygon
     layer = iface.activeLayer()
+    #TODO: allow multiple selection
     # only want the first feature
     feature = layer.selectedFeatures()[0]
+    #TODO: add feature id. format as comma sep csv. add bounds description -> min east    
     # get bounding box as string
     bbox = feature.geometry().boundingBox().toString()
     
     # write contents to file
+    #TODO: add coordinate system used & total number features selected. timestamped utc
     f = open(bboxCoordFile,"w")
     f.write(bbox)
     f.close()
